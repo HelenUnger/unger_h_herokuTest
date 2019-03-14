b73b26d87618672f193f63f1925a3d0f1c467753
Jeremy <helen-unger@hotmail.com>
2019-03-14 17:39:12 -0400

    working with routes and data object

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    name: "helen",
+    role: "admin",
+
+    user1: {
+        name: "user1",
+        role: "designer",
+        bio: "weird stuff",
+        bioPic: "user1.png"
+    },
+
+    user2: {
+        name: "user2",
+        role: "developer",
+        bio: "nerd alert",
+        bioPic: "user2.png"
+    }
+}
+
+module.exports = data;
